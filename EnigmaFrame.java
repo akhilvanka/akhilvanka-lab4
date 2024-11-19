8b1ee07a2f0ad459dd2d9c8e46844f89f5f5ee5e
Akhil Vanka <dev+github@akhilv.me>
2024-11-19 14:53:01 -0500

    Add Setting and Enigma tabPanel for views

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -22,14 +22,39 @@ public class EnigmaFrame extends JFrame {
         encryptDecryptPanel.add(encryptButton);
         encryptDecryptPanel.add(decryptButton);
 
-        // Text Area
         JTextArea encryptTextField = new JTextArea();
         JTextArea decryptTextField = new JTextArea();
+        JPanel encryptPanel = new JPanel(new BorderLayout());
+        JPanel decryptPanel = new JPanel(new BorderLayout());
+        encryptPanel.add(new JLabel(" Encrypt: "), BorderLayout.WEST);
+        encryptPanel.add(encryptTextField);
+        decryptPanel.add(new JLabel(" Decrypt: "), BorderLayout.WEST);
+        decryptPanel.add(decryptTextField);
 
-        page1.add(encryptTextField);
+        page1.add(encryptPanel);
         page1.add(encryptDecryptPanel);
-        page1.add(decryptTextField);
+        page1.add(decryptPanel);
         
+
+        // MARK: PAGE 2 
+        String rotors[] = {"1", "2", "3", "4", "5"};
+        JPanel settingsPanel = new JPanel(new FlowLayout());
+        JTextField initialPositionField = new JTextField(5);
+        JComboBox<String> innerComboBox = new JComboBox<String>(rotors);
+        JComboBox<String> middleComboBox = new JComboBox<String>(rotors);
+        JComboBox<String> outerComboBox = new JComboBox<String>(rotors);
+
+
+        settingsPanel.add(new JLabel("Inner:"));
+        settingsPanel.add(innerComboBox);
+        settingsPanel.add(new JLabel("Middle:"));
+        settingsPanel.add(middleComboBox);
+        settingsPanel.add(new JLabel("Outer:"));
+        settingsPanel.add(outerComboBox);
+        settingsPanel.add(new JLabel("Initial Position:"));
+        settingsPanel.add(initialPositionField);
+
+        page2.add(settingsPanel, BorderLayout.NORTH);
 
         tabPanel.addTab("Enigma", page1); 
         tabPanel.addTab("Settings", page2); 
