f9c2de1c5de85185e3fc60abe2891f38001731ff
Akhil Vanka <dev+github@akhilv.me>
2024-11-19 14:28:24 -0500

    Create initial GUI and JFrame

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -1,6 +1,41 @@
 import java.io.*;
 import java.util.*;
 import javax.swing.*;
+import java.awt.*;
 
 public class EnigmaFrame extends JFrame {
+
+    // Construct JFRAME
+    public EnigmaFrame() {
+        setTitle("Enigma Machine");
+        JTabbedPane tabPanel = new JTabbedPane();   
+
+        JPanel page1 = new JPanel(); 
+        page1.setLayout(new BoxLayout(page1, BoxLayout.Y_AXIS));
+        JPanel page2 = new JPanel(new BorderLayout()); 
+
+
+        // MARK: PAGE 1
+        JPanel encryptDecryptPanel = new JPanel(new FlowLayout());
+        JButton encryptButton = new JButton("Encrypt");
+        JButton decryptButton = new JButton("Decrypt");
+        encryptDecryptPanel.add(encryptButton);
+        encryptDecryptPanel.add(decryptButton);
+
+        // Text Area
+        JTextArea encryptTextField = new JTextArea();
+        JTextArea decryptTextField = new JTextArea();
+
+        page1.add(encryptTextField);
+        page1.add(encryptDecryptPanel);
+        page1.add(decryptTextField);
+        
+
+        tabPanel.addTab("Enigma", page1); 
+        tabPanel.addTab("Settings", page2); 
+
+        add(tabPanel);
+    }
+
+
 }
--- EnigmaGUI.java
+++ EnigmaGUI.java
@@ -4,7 +4,8 @@ import javax.swing.*;
 
 public class EnigmaGUI {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Enigma Machine");
+        EnigmaFrame frame = new EnigmaFrame();
+        
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 400);
         frame.setVisible(true);
