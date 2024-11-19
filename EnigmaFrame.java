199d9b2462962708949f0297e60b32f077563a14
Akhil Vanka <dev+github@akhilv.me>
2024-11-19 15:32:54 -0500

    Add Listener to handle Enigma encrypting

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -2,11 +2,22 @@ import java.io.*;
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class EnigmaFrame extends JFrame {
 
+    private JTextArea decryptTextField;
+    private JTextArea encryptTextField;
+
+    private JComboBox<String> innerComboBox;
+    private JComboBox<String> middleComboBox;
+    private JComboBox<String> outerComboBox;
+
+    private JTextField initialPositionField;
+
     // Construct JFRAME
     public EnigmaFrame() {
+        super();
         setTitle("Enigma Machine");
         JTabbedPane tabPanel = new JTabbedPane();   
 
@@ -14,16 +25,18 @@ public class EnigmaFrame extends JFrame {
         page1.setLayout(new BoxLayout(page1, BoxLayout.Y_AXIS));
         JPanel page2 = new JPanel(new BorderLayout()); 
 
-
         // MARK: PAGE 1
         JPanel encryptDecryptPanel = new JPanel(new FlowLayout());
         JButton encryptButton = new JButton("Encrypt");
         JButton decryptButton = new JButton("Decrypt");
+        EnigmaListener listener = new EnigmaListener();
+        encryptButton.addActionListener(listener);
+        decryptButton.addActionListener(listener);
         encryptDecryptPanel.add(encryptButton);
         encryptDecryptPanel.add(decryptButton);
 
-        JTextArea encryptTextField = new JTextArea();
-        JTextArea decryptTextField = new JTextArea();
+        encryptTextField = new JTextArea();
+        decryptTextField = new JTextArea();
         JPanel encryptPanel = new JPanel(new BorderLayout());
         JPanel decryptPanel = new JPanel(new BorderLayout());
         encryptPanel.add(new JLabel(" Encrypt: "), BorderLayout.WEST);
@@ -39,10 +52,10 @@ public class EnigmaFrame extends JFrame {
         // MARK: PAGE 2 
         String rotors[] = {"1", "2", "3", "4", "5"};
         JPanel settingsPanel = new JPanel(new FlowLayout());
-        JTextField initialPositionField = new JTextField(5);
-        JComboBox<String> innerComboBox = new JComboBox<String>(rotors);
-        JComboBox<String> middleComboBox = new JComboBox<String>(rotors);
-        JComboBox<String> outerComboBox = new JComboBox<String>(rotors);
+        initialPositionField = new JTextField(5);
+        innerComboBox = new JComboBox<String>(rotors);
+        middleComboBox = new JComboBox<String>(rotors);
+        outerComboBox = new JComboBox<String>(rotors);
 
 
         settingsPanel.add(new JLabel("Inner:"));
@@ -61,6 +74,28 @@ public class EnigmaFrame extends JFrame {
 
         add(tabPanel);
     }
+
+    private class EnigmaListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            int inner = Integer.parseInt((String) innerComboBox.getSelectedItem());
+            int middle = Integer.parseInt((String) middleComboBox.getSelectedItem());
+            int outer = Integer.parseInt((String) outerComboBox.getSelectedItem());
+            String initialPosition = initialPositionField.getText();
+            String input = encryptTextField.getText();
+            String output = decryptTextField.getText();
+
+            Enigma enigma = new Enigma(inner, middle, outer, initialPosition);
+
+            if (e.getActionCommand().equals("Encrypt")) {
+                String encryptedMessage = enigma.encrypt(input);
+                decryptTextField.setText(encryptedMessage);
+            } else if (e.getActionCommand().equals("Decrypt")) {
+                String decryptedMessage = enigma.decrypt(output);
+                encryptTextField.setText(decryptedMessage);
+            }
+        }
+   }
 
 
 }
