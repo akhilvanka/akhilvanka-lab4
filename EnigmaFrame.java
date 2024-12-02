65ae5925c450237dbd3293574a9a87c99f482f03
Akhil Vanka <dev+github@akhilv.me>
2024-12-01 21:34:52 -0500

    Remove unused imports and spacing

--- EnigmaFrame.java
+++ EnigmaFrame.java
@@ -1,5 +1,3 @@
-import java.io.*;
-import java.util.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
--- EnigmaGUI.java
+++ EnigmaGUI.java
@@ -1,11 +1,8 @@
-import java.io.*;
-import java.util.*;
 import javax.swing.*;
 
 public class EnigmaGUI {
     public static void main(String[] args) {
         EnigmaFrame frame = new EnigmaFrame();
-        
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 400);
         frame.setVisible(true);
